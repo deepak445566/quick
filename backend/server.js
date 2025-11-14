748a27ab123df852fab1e01526bd2473c237c77a
deepak445566 <deepakpandey1212d@gmail.com>
2025-11-14 13:40:45 +0530

    last

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://dainty-figolla-0f3ef6.netlify.app',
   credentials: true
 }));
 
--- quick/src/context/AuthContext.jsx
+++ quick/src/context/AuthContext.jsx
@@ -16,7 +16,7 @@ export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.defaults.baseURL = 'http://localhost:5000/api';
+    axios.defaults.baseURL = 'https://quick-4p16.onrender.com/api';
     axios.defaults.withCredentials = true;
   }, []);
 
